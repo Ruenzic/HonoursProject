d8373833776546a5956730b27afb645d3510e4ec
dbnjos001 <dbnjos001@cs.uct.ac.za>
2016-09-06 13:38:13 +0200

    debugging

--- IPUpdater.py
+++ IPUpdater.py
@@ -8,7 +8,7 @@ import os
 
 f = os.popen('ifconfig wlan0 | grep "inet\ addr" | cut -d: -f2 | cut -d" " -f1')
 addr = str(f.read().strip())
-print(addr)
+print(addr) #was working with this here? 
 
 gg = Simplegist(username='Ruenzic', api_token='4d794eba5326a8e3b0d552f7a78ed91742cc7325')
 current_time = str(datetime.now())
