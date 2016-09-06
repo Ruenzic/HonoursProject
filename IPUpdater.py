ac822e8e0bea44d6fb3db249a5156f1f9808645e
dbnjos001 <dbnjos001@cs.uct.ac.za>
2016-09-06 11:17:05 +0200

    Added Files for ip updater

---
+++ IPUpdater.py
@@ -0,0 +1,16 @@
+
+from datetime import datetime
+from simplegist import Simplegist
+from urllib2 import urlopen
+
+
+
+
+
+
+
+gg = Simplegist(username='Ruenzic', api_token='4d794eba5326a8e3b0d552f7a78ed91742cc7325')
+current_time = str(datetime.now())
+ip = urlopen('http://ipinfo.io/ip').read().strip()
+content = current_time + "   " + ip
+gg.profile().edit(id='1a9229e4de7ebaa8cc617163a52ced2f', content=content)
