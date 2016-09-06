5b2ebcfc29f16072cdf9a08c53cbe352a07a36c8
dbnjos001 <dbnjos001@cs.uct.ac.za>
2016-09-06 13:27:42 +0200

    more ip debugging..

--- IPUpdater.py
+++ IPUpdater.py
@@ -8,10 +8,12 @@ import os
 
 f = os.popen('ifconfig wlan0 | grep "inet\ addr" | cut -d: -f2 | cut -d" " -f1')
 addr = str(f.read().strip())
-print(addr)
+
 
 gg = Simplegist(username='Ruenzic', api_token='4d794eba5326a8e3b0d552f7a78ed91742cc7325')
 current_time = str(datetime.now())
 ip = urlopen('http://ipinfo.io/ip').read().strip()
 content = current_time + "   " + addr
 gg.profile().edit(id='1a9229e4de7ebaa8cc617163a52ced2f', content=content)
+
+print(content)
