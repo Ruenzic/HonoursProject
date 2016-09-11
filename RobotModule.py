325e441f190e0de4782f02abea51a673f664fcb9
Josh <josh57@live.co.za>
2016-09-11 12:59:58 +0200

    made change to follow_path when going straight after turning

--- RobotModule.py
+++ RobotModule.py
@@ -426,7 +426,8 @@ class Robot:
                 if (current_rot != rot1 and current_rot != rot2):
                     print("changing rot, from " + str(current_rot) + " to " + str(rot1))
                     self.change_rot(current_rot, rot1)
-                if (current_rot == rot1):
+                    self.forward(1)
+                elif (current_rot == rot1):
                     self.forward(1)
                 elif (current_rot == rot2):
                     self.reverse(1)
