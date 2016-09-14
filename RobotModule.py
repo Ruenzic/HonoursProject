61d0180ae5f0742ff9e728c81acf3c10130c56b1
dbnjos001 <dbnjos001@cs.uct.ac.za>
2016-09-14 22:19:39 +0200

    Made Robot Class a singleton class

--- RobotModule.py
+++ RobotModule.py
@@ -6,7 +6,20 @@ import time
 from Node import *
 
 
-class Robot:
+class _Singleton(type):
+    """ A metaclass that creates a Singleton base class when called. """
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(_Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+class Singleton(_Singleton('SingletonMeta', (object,), {})): pass
+
+
+class Robot(Singleton):
+    pass
+
 
     __RIGHT_PWM_PIN = 10
     __RIGHT_1_PIN = 10
