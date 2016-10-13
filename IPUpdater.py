d031838be90718c8a38e244929fa88b1f2e3055b
Josh <josh57@live.co.za>
2016-10-13 11:25:26 +0200

    Updated API Token for IpUpdater

--- IPUpdater.py
+++ IPUpdater.py
@@ -10,7 +10,7 @@ f = os.popen('ifconfig wlan0 | grep "inet\ addr" | cut -d: -f2 | cut -d" " -f1')
 addr = str(f.read().strip())
 #print(addr)
 
-gg = Simplegist(username='Ruenzic', api_token='4d794eba5326a8e3b0d552f7a78ed91742cc7325')
+gg = Simplegist(username='Ruenzic', api_token='fc29144837eaab82e9835dc9a31ad7e75252580c')
 current_time = str(datetime.now())
 ip = urlopen('http://ipinfo.io/ip').read().strip()
 content = current_time + "   " + addr
