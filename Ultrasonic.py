3eeed2e8df3168bd3975cefc88a3e9d2c4f12118
JeremyC-za <jcoupland12@gmail.com>
2017-10-24 10:17:09 +0200

    Update Ultrasonic.py

--- Ultrasonic.py
+++ Ultrasonic.py
@@ -1,8 +1,8 @@
 import RPi.GPIO as GPIO
 import time
 
-#Author: Jeremy Coupland
-#Last updated: 2016/09/15
+# Author: Jeremy Coupland
+# Last updated: 2016/09/15
 
 class Ultrasonic:
 
@@ -14,38 +14,36 @@ class Ultrasonic:
         GPIO.setwarnings(False)
         GPIO.cleanup()
 
-
     def get_distance(self):
         GPIO.setmode(GPIO.BOARD)
         GPIO.setwarnings(False)
 
-        GPIO.setup(self.__TRIG,GPIO.OUT) #Set trigger as output
-        GPIO.setup(self.__ECHO,GPIO.IN)  #Set echo as input
+        GPIO.setup(self.__TRIG,GPIO.OUT) # Set trigger as output
+        GPIO.setup(self.__ECHO,GPIO.IN) # Set echo as input
 
-        GPIO.output(self.__TRIG, False)  #Make sure trigger isn't sending anything
-        time.sleep(1)       #Let the sensor settle
+        GPIO.output(self.__TRIG, False) # Make sure trigger isn't sending anything
+        time.sleep(1) # Let the sensor settle
 
-        distances = 0   #Distance initialized to 0
-        for i in range (0,3):   #Take average of 3 measurements
+        distances = 0 # Distance initialized to 0
+        for i in range (0,3): # Take average of 3 measurements
             GPIO.output(self.__TRIG, True)
-            time.sleep(0.00001)         #Pulse trigger for 10 microseconds
+            time.sleep(0.00001) # Pulse trigger for 10 microseconds
             GPIO.output(self.__TRIG, False)
             start = time.time()
           
             while GPIO.input(self.__ECHO)==0:
-              start = time.time()   #Start of the pulse
+              start = time.time() # Start of the pulse
 
             while GPIO.input(self.__ECHO)==1:
-              stop = time.time()    #End of the pulse
+              stop = time.time() # End of the pulse
 
-            elapsed = stop-start    #Length of the pulse
-            distance = (elapsed * 34300)/2    #Manipulating the Distance = Speed * Time equation
+            elapsed = stop-start  # Length of the pulse
+            distance = (elapsed * 34300)/2 # Manipulating the Distance = Speed * Time equation
 
-            distances = distances + distance #Keeping a running total of the measured distances
+            distances = distances + distance # Keeping a running total of the measured distances
 
-            time.sleep(0.1) #Brief wait between next measurement
+            time.sleep(0.1) # Brief wait between next measurement
 
-        distances = distances/3     #Average distance using the three measurements
+        distances = distances/3 # Average distance using the three measurements
 
-        return round(distances,2) #Distance in centimeters, rounded to 2 decimal places
-
+        return round(distances,2) # Distance in centimeters, rounded to 2 decimal places
